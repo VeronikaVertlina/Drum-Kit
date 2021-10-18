8fcdbbefb303c4bf9e675dfff5cf0241eb4cb1bc
ronya78@gmail.com <ronya78@gmail.com>
2021-10-18 19:32:14 +0300

    Add code

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
-for (var i = 0; i < numberOfDrumButtons; i++){
 
-  document.querySelectorAll(".drum")[i].addEventListener("click", function handleClick() {
-    alert("I got clicked!");
+for (var i = 0; i < numberOfDrumButtons; i++){
+  document.querySelectorAll(".drum")[i].addEventListener("click", function play() {
+     // alert("I got clicked!");
+     this.style.color = "white";
 
     //what to do when click detected.
   });
 }
 
-
+// var audio = new Audio (
+//   'sounds/tom-1.mp3',
+//   'sounds/tom-2.mp3',
+//   'sounds/tom-3.mp3',
+//   'sounds/tom-4.mp3',
+//   'sounds/snare.mp3',
+//   'sounds/crash.mp3',
+//   'sounds/kick-bass.mp3'
+// );
+//
+// audio.play();
 
 // --------------- It's my code -----------------------
 // document.querySelector(".set").addEventListener("click", handleClick);
